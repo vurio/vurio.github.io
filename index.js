8a0ab26d6924039f63d5a57fec5b9a6e5014b852
vadmentt <64926114+vadmentt@users.noreply.github.com>
2020-06-14 11:37:02 -0700

    2 characters
    added 2 characters as well as improved the animations and touch detection

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var character = document.getElementById("character");
 var block = document.getElementById("block");
+var blockClone = block.cloneNode(true);
+var iDidnt = function() {
+    location.reload();
+}
+var point = 0;
+var x = document.getElementById("x");
+setTimeout(function() {document.getElementById("game").appendChild(blockClone);}, 1000);
+blockClone.style.top = "90px";
 function jump() {
     if (character.classList != "animate") {
     character.classList.add("animate");
@@ -11,22 +19,28 @@ function jump() {
 var commitDie = setInterval(function(){
     var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if (blockLeft < 70 && blockLeft > 50 && characterTop >= 130) {
+    if (blockLeft < 70 && blockLeft > 50 && characterTop >= 110) {
         block.style.animation = "none";
         block.style.display = "none";
+        blockClone.style.animation = "none";
+        blockClone.style.display = "none";
         //alert("u ded");
         document.getElementById("t").innerHTML = "You are bad at this game, and it doesn't have to do with how trash this is.";
     }
 
-}, 10);
+}, 5);
+var getPoint = setInterval(function(){
+    var characterLeft = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if (characterLeft >= blockLeft) {
+        point ++;
+        //console.log(point);
+        document.getElementById("point").innerHTML = point;
+    }
+}, 130);
 document.body.onkeyup = function(e){
     if(e.keyCode == 32){
         jump();
     }
 }
-var iDidnt = function() {
-    location.reload();
-}
-var point = 0;
-var x = document.getElementById("x");
 x.play();
