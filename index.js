5324e5e868d0991ad8a246428c1e715840246a9c
vadmentt <64926114+vadmentt@users.noreply.github.com>
2020-06-14 23:23:48 -0700

    crouch icon and bug fixes
    -Fixed point bugs
    -Fixed colision detection bugs
    -Added crouch
    -Added keyboard controls
    -Added .ico

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var character = document.getElementById("character");
 var block = document.getElementById("block");
+//remove later
+block.style.top = "110px";
 var blockClone = block.cloneNode(true);
 var iDidnt = function() {
     location.reload();
@@ -20,6 +22,9 @@ var commitDie = setInterval(function(){
     var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     if (blockLeft < 70 && blockLeft > 50 && characterTop >= 110) {
+        if (characterTop == 180 && blockLeft < 70 && blockLeft > 50) {
+            return;
+        }
         block.style.animation = "none";
         block.style.display = "none";
         blockClone.style.animation = "none";
@@ -37,10 +42,24 @@ var getPoint = setInterval(function(){
         //console.log(point);
         document.getElementById("point").innerHTML = point;
     }
-}, 130);
+}, 110);
 document.body.onkeyup = function(e){
     if(e.keyCode == 32){
         jump();
     }
+    if (e.keyCode == 16){
+        down();
+    }
+    if (e.keyCode == 82){
+        iDidnt();
+    }
+}
+function down() {
+    if (character.classList != "crouch") {
+    character.classList.add("crouch");
+    }
+    setTimeout(function(){
+        character.classList.remove("crouch");
+    }, 500);
 }
 x.play();
