45f715fbd45101feb392798ba7108f8625967f6e
vadmentt <64926114+vadmentt@users.noreply.github.com>
2020-06-24 13:44:54 -0700

    A lot
    -new page

---
+++ school.js
@@ -0,0 +1,6 @@
+var turnOn = function() {
+    document.getElementById("map").setAttribute('src', "https://www.mapsofindia.com/worldmap/countries-capitals.html");
+}
+var turnOff = function() {
+    document.getElementById("map").setAttribute('src', "about:blank");
+}
