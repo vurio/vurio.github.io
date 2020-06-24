d453ea9e676fa744472fa1189f051a1f29771036
vadmentt <64926114+vadmentt@users.noreply.github.com>
2020-06-24 16:21:46 -0700

    more features
    turn off buttons

--- second_index.js
+++ second_index.js
@@ -3,6 +3,9 @@ var update = function() {
     console.log(document.getElementById("hyper").value)
     yes.setAttribute('src', document.getElementById("hyper").value);
 }
+var stop = function() {
+    yes.setAttribute('src', "about:blank");
+}
 var playMusic = function() {
     document.getElementById("y").play();
 }
